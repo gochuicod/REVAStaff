40fd4c1ae9557023ac940c8c6227a4f4e93a04bf
gochuicod <gochuicodarelleq@gmail.com>
2024-06-24 20:34:10 +0800

    added login api

---
+++ frontend/src/api/login.ts
@@ -0,0 +1,11 @@
+import axios from './axiosConfig';
+
+export interface ILogin {
+  username: string;
+  password: string;
+}
+
+export const Login = async (data: { username: string, password: string}): Promise<ILogin> => {
+  const response = await axios.post<ILogin>('auth/login/', data)  
+  return response.data
+}
