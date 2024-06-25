f06d2219459365d1f409d57a48323b1d459e2d0c
gochuicod <gochuicodarelleq@gmail.com>
2024-06-25 20:53:57 +0800

    Updated pages layout

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,7 +1,18 @@
 import { Outlet } from "react-router-dom";
+import { Sidebar, Header } from "../components";
 
 const Layout = () => {
-  return <Outlet/>
+  return (
+    <div className="flex flex-row">
+      <div>
+        <Sidebar/>
+      </div>
+      <div className="flex flex-col h-screen lg:p-[2vw] md:p-[5vw] bg-black lg:rounded-l-[2vw] md:rounded-l-[4vw] lg:w-[94vw] md:w-[90vw] overflow-y-scroll">
+        <Header/>
+        <Outlet/>
+      </div>
+    </div>
+  )
 }
 
 export default Layout;
