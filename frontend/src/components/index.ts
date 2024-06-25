df0c4deb8c98c1732c48747877594d08f2dee4f0
gochuicod <gochuicodarelleq@gmail.com>
2024-06-25 14:32:42 +0800

    Updated indexes

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Loader } from "./Loader";
+export { default as LoginForm } from "./LoginForm";
+export { default as ProtectedRoute } from "./ProtectedRoute";
+export { default as PublicRoute } from "./PublicRoute";
---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,3 @@
+export { default as Login } from "./Login";
+export { default as Error } from "./Error";
+export { default as Dashboard } from "./Dashboard";
