f26449214871c9130a80c1ca96380ae54c3ef4a7
gochuicod <gochuicodarelleq@gmail.com>
2024-06-25 14:32:22 +0800

    Updated Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
-import Login from "./pages/Login"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import 'react-toastify/dist/ReactToastify.css';
 
-const App = () => {
-  return (
-    <Login/>
-  )
-}
+import { Login, Error, Dashboard } from "./pages";
+
+import { ProtectedRoute, PublicRoute } from "./components";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ProtectedRoute element={<Dashboard/>} />,
+    errorElement: <Error/>
+  },
+  {
+    path: "/login",
+    element: <PublicRoute element={<Login/>}/>,
+    errorElement: <Error/>,
+  },
+])
+
+const App = () => <RouterProvider router={router}/>
 
 export default App
