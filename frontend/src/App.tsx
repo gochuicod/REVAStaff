17efc1c3753afa7a540deadeb4b54bcf05fce7c1
gochuicod <gochuicodarelleq@gmail.com>
2024-06-25 20:52:44 +0800

    Added builder route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,25 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import 'react-toastify/dist/ReactToastify.css';
 
-import { Login, Error, Dashboard } from "./pages";
+import { Login, Error, Dashboard, Layout, Builder } from "./pages";
 
 import { ProtectedRoute, PublicRoute } from "./components";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <ProtectedRoute element={<Dashboard/>} />,
-    errorElement: <Error/>
+    element: <ProtectedRoute element={<Layout/>} />,
+    errorElement: <Error/>,
+    children: [
+      {
+        index: true,
+        element: <Dashboard/>
+      },
+      {
+        path: "builder",
+        element: <Builder/>
+      },
+    ]
   },
   {
     path: "/login",
