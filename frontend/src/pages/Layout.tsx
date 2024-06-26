fa28f15f1c59e5a1ba79577e2150e81e5081fbc8
gochuicod <gochuicodarelleq@gmail.com>
2024-06-26 21:29:42 +0800

    Sidebar update

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar, Header } from "../components";
+import { useSidebarContext } from "../context/SidebarContext";
 
 const Layout = () => {
+  const { sidebarStatus } = useSidebarContext()
+
   return (
     <div className="flex flex-row">
       <div>
         <Sidebar/>
       </div>
-      <div className="flex flex-col h-screen lg:p-[2vw] md:p-[5vw] bg-black lg:rounded-l-[2vw] md:rounded-l-[4vw] lg:w-[94vw] md:w-[90vw] overflow-y-scroll">
+      <div className={`flex flex-col h-screen lg:p-[2vw] md:p-[3vw] p-[5vw] bg-black lg:rounded-l-[2vw] md:rounded-l-[4vw] ${sidebarStatus === "opened" ? "rounded-l-[5vw]" : "rounded-l-[0vw]"} lg:w-[94vw] md:w-[90vw] w-[100vw] overflow-y-scroll`}>
         <Header/>
         <Outlet/>
       </div>
@@ -15,4 +18,4 @@ const Layout = () => {
   )
 }
 
-export default Layout;
+export default Layout; 
