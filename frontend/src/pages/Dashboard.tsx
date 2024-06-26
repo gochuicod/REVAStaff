4c2be9b55dc89ba3eb6189c3dddf489d9a384a49
gochuicod <gochuicodarelleq@gmail.com>
2024-06-26 21:29:22 +0800

    Temporary dashboard template

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,6 +1,126 @@
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis } from 'recharts';
+
+const radardata = [
+  {
+    subject: 'Math',
+    A: 120,
+    B: 110,
+    fullMark: 150,
+  },
+  {
+    subject: 'Chinese',
+    A: 98,
+    B: 130,
+    fullMark: 150,
+  },
+  {
+    subject: 'English',
+    A: 86,
+    B: 130,
+    fullMark: 150,
+  },
+  {
+    subject: 'Geography',
+    A: 99,
+    B: 100,
+    fullMark: 150,
+  },
+  {
+    subject: 'Physics',
+    A: 85,
+    B: 90,
+    fullMark: 150,
+  },
+  {
+    subject: 'History',
+    A: 65,
+    B: 85,
+    fullMark: 150,
+  },
+];
+
+const areadata = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
 const Dashboard = () => {
   return (
-    <h1 className="text-white">This is the Dashboard</h1>
+    <div className="flex flex-col lg:mt-[2vw] md:mt-[3vw] mt-[5vw] gap-[2vw]">
+      <div className="flex lg:flex-row md:flex-col flex-col justify-between gap-x-[2vw] lg:gap-y-[0vw] md:gap-y-[3vw] gap-y-[5vw]">
+        <div className="flex justify-center items-center bg-[#1D1E20] text-white lg:rounded-[2vw] md:rounded-[2vw] rounded-[5vw] h-[49vh] lg:w-[50%] md:w-[100%] w-full lg:p-[1vw] md:p-[1vw] p-[5vw]">
+          <ResponsiveContainer width="100%" height="100%">
+            <AreaChart
+              width={500}
+              height={400}
+              data={areadata}
+              margin={{
+                top: 10,
+                right: 30,
+                left: 0,
+                bottom: 0,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Area type="monotone" dataKey="uv" stroke="#FFEE99" fill="#FED500" />
+            </AreaChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="flex justify-center items-center bg-[#1D1E20] text-white lg:rounded-[2vw] md:rounded-[2vw] rounded-[5vw] h-[49vh] lg:w-[50%] md:w-[100%] w-full lg:p-[1vw] md:p-[1vw] p-[5vw]">
+          <ResponsiveContainer width="100%" height="100%">
+            <RadarChart cx="50%" cy="50%" outerRadius="80%" data={radardata}>
+              <PolarGrid />
+              <PolarAngleAxis dataKey="subject" />
+              <PolarRadiusAxis />
+              <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+            </RadarChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
+    </div>
   )
 };
 
