73070f87ca33eb1a29fe752898f27c78090a0ae0
gochuicod <gochuicodarelleq@gmail.com>
2024-06-27 12:26:38 +0800

    Test deploy #20

--- backend/app/routes/user_router.py
+++ backend/app/routes/user_router.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
+from app.models.user import User
 
 router = APIRouter()
 
 @router.get("/")
 async def read_users():
-  return {"message": "List of users"}
+  users = await User.find_all().to_list();
+  return users
 
 @router.get("/{user_id}")
 async def read_user(user_id: str):
