b55d23dba93ec4d65f685a57684b534c713adf9f
gochuicod <gochuicodarelleq@gmail.com>
2024-06-27 10:11:22 +0800

    Test deploy #3

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,44 @@
-import os
+from os import getenv
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from typing import Callable
 
-import uvicorn
-from dotenv import load_dotenv
+from src.config.database import init_db
+from src.routes.user_router import user
+from src.routes.authentication_router import authentication
 
-load_dotenv()
-RELOAD = bool(os.getenv('RELOAD'))
+app = FastAPI(
+  title="REVAStaff API",
+  description="This API backend powers a sophisticated wiki platform for REVAStaff, \
+            designed to streamline knowledge management within the organization. \
+            It provides robust API endpoints for creating, editing, and retrieving \
+            wiki documents securely. Built with efficiency in mind, it ensures seamless \
+            integration with user authentication and authorization systems, guaranteeing \
+            only authorized personnel access critical information. The backend leverages \
+            modern encryption standards and database technologies to maintain data integrity \
+            and confidentiality, supporting REVAStaff's commitment to efficient collaboration \
+            and knowledge sharing.",
+  version="1.0",
+)
+
+origins = getenv("ORIGINS", "*")
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=[origins] if origins != "*" else ["*"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
+@app.middleware("http")
+async def db_session_middleware(request: Request, call_next: Callable):
+  await init_db()
+  response = await call_next(request)
+  return response
+
+app.include_router(user, tags=["Users"], prefix="/api/users")
+app.include_router(authentication, tags=["Authentication"], prefix="/api/auth")
 
 if __name__ == "__main__":
-  uvicorn.run("src.app:app", reload=RELOAD)
+  import uvicorn
+  uvicorn.run(app, host="0.0.0.0", port=8000)
--- backend/src/app.py
+++ backend/src/app.py
@@ -18,92 +18,46 @@
 """
 
 # from os import getenv
-
-# from fastapi import FastAPI
+# from fastapi import FastAPI, Request
 # from fastapi.middleware.cors import CORSMiddleware
-# from contextlib import asynccontextmanager
+# from typing import Callable
 
 # from src.config.database import init_db
-
 # from src.routes.user_router import user
 # from src.routes.authentication_router import authentication
 
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     await init_db()
-#     print("Database initialized")
-#     yield
-#     print("Application is shutting down")
-
 # app = FastAPI(
 #   title="REVAStaff API",
-  # description="This API backend powers a sophisticated wiki platform for REVAStaff, \
-  #             designed to streamline knowledge management within the organization. \
-  #             It provides robust API endpoints for creating, editing, and retrieving \
-  #             wiki documents securely. Built with efficiency in mind, it ensures seamless \
-  #             integration with user authentication and authorization systems, guaranteeing \
-  #             only authorized personnel access critical information. The backend leverages \
-  #             modern encryption standards and database technologies to maintain data integrity \
-  #             and confidentiality, supporting REVAStaff's commitment to efficient collaboration \
-  #             and knowledge sharing.",
+#   description="This API backend powers a sophisticated wiki platform for REVAStaff, \
+#             designed to streamline knowledge management within the organization. \
+#             It provides robust API endpoints for creating, editing, and retrieving \
+#             wiki documents securely. Built with efficiency in mind, it ensures seamless \
+#             integration with user authentication and authorization systems, guaranteeing \
+#             only authorized personnel access critical information. The backend leverages \
+#             modern encryption standards and database technologies to maintain data integrity \
+#             and confidentiality, supporting REVAStaff's commitment to efficient collaboration \
+#             and knowledge sharing.",
 #   version="1.0",
-#   lifespan=lifespan
 # )
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=[getenv("ORIGINS")],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# app.include_router(user, tags=["Users"], prefix="/api/users")
-# app.include_router(authentication, tags=["Authentication"], prefix="/api/auth")
-
-from os import getenv
-
-from fastapi import FastAPI, Request
-from fastapi.middleware.cors import CORSMiddleware
-from typing import Callable
-
-from src.config.database import init_db
-from src.routes.user_router import user
-from src.routes.authentication_router import authentication
-
-app = FastAPI(
-  title="REVAStaff API",
-  description="This API backend powers a sophisticated wiki platform for REVAStaff, \
-              designed to streamline knowledge management within the organization. \
-              It provides robust API endpoints for creating, editing, and retrieving \
-              wiki documents securely. Built with efficiency in mind, it ensures seamless \
-              integration with user authentication and authorization systems, guaranteeing \
-              only authorized personnel access critical information. The backend leverages \
-              modern encryption standards and database technologies to maintain data integrity \
-              and confidentiality, supporting REVAStaff's commitment to efficient collaboration \
-              and knowledge sharing.",
-  version="1.0",
-)
-
-app.get('/')
-async def test() -> dict:
-  return { "message": "Hello World!" }
 
 # origins = getenv("ORIGINS", "*")
-
 # app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=[origins] if origins != "*" else ["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
+#   CORSMiddleware,
+#   allow_origins=[origins] if origins != "*" else ["*"],
+#   allow_credentials=True,
+#   allow_methods=["*"],
+#   allow_headers=["*"],
 # )
 
 # @app.middleware("http")
 # async def db_session_middleware(request: Request, call_next: Callable):
-#     await init_db()
-#     response = await call_next(request)
-#     return response
+#   await init_db()
+#   response = await call_next(request)
+#   return response
 
 # app.include_router(user, tags=["Users"], prefix="/api/users")
 # app.include_router(authentication, tags=["Authentication"], prefix="/api/auth")
+
+# if __name__ == "__main__":
+#   import uvicorn
+#   uvicorn.run(app, host="0.0.0.0", port=8000)
