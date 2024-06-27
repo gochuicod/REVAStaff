7edca1b2ff8f619a82c2470acb84c541b09c8b03
gochuicod <gochuicodarelleq@gmail.com>
2024-06-27 11:32:49 +0800

    Test deploy #16

--- backend/config/database.py
+++ backend/config/database.py
@@ -26,34 +26,81 @@
   - `models.auth`: Contains the `LoginRequestBody` model.
 """
 
-from os import environ
-from fastapi import HTTPException, status
-from motor.motor_asyncio import AsyncIOMotorClient
+# from os import environ
+# from fastapi import HTTPException, status
+# from motor.motor_asyncio import AsyncIOMotorClient
+# from beanie import init_beanie
+
+# from models.user import User
+# from models.auth import LoginRequestBody
+
+# MONGODB_URI = environ.get('MONGODB_URI')
+# MONGODB_NAME = environ.get('MONGODB_NAME')
+
+# if MONGODB_URI is None or MONGODB_NAME is None:
+#   raise HTTPException(
+#     status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+#     detail='Please define the MONGODB_URI and MONGODB_NAME environment variable inside .env'
+#   )
+
+# async def init_db():
+#   try:
+#     client = AsyncIOMotorClient(MONGODB_URI)
+#     db = client[MONGODB_NAME]
+
+#     await init_beanie(
+#       database=db,
+#       document_models=[
+#         User,
+#         LoginRequestBody
+#       ]
+#     )
+#   except Exception as e:
+#     print(e)
+
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+
+import os
+
 from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+from fastapi import HTTPException, status
 
 from models.user import User
 from models.auth import LoginRequestBody
 
-MONGODB_URI = environ.get('MONGODB_URI')
-MONGODB_NAME = environ.get('MONGODB_NAME')
+MONGODB_URI = os.environ.get('MONGODB_URI')
+MONGODB_NAME = os.environ.get('MONGODB_NAME')
 
 if MONGODB_URI is None or MONGODB_NAME is None:
-  raise HTTPException(
-    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-    detail='Please define the MONGODB_URI and MONGODB_NAME environment variable inside .env'
-  )
+    raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                        detail='Please define the MONGODB_URI and MONGODB_NAME environment variable inside .env')
+
 
 async def init_db():
-  try:
-    client = AsyncIOMotorClient(MONGODB_URI)
-    db = client[MONGODB_NAME]
+    """
+    The `init_db` function initializes the database and sets up the document models.
+    """
+    try:
+        client = AsyncIOMotorClient(MONGODB_URI)
+        db = client[MONGODB_NAME]
 
-    await init_beanie(
-      database=db,
-      document_models=[
-        User,
-        LoginRequestBody
-      ]
-    )
-  except Exception as e:
-    print(e)
+        await init_beanie(
+            database=db,
+            document_models=[
+                User,
+                LoginRequestBody
+            ],
+        )
+    except Exception as e:
+        print(e)
