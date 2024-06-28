8f380bdde135e8869e3601633a56961369108821
gochuicod <gochuicodarelleq@gmail.com>
2024-06-28 19:50:03 +0800

    Attempt CORS fix #14

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import connectDB from './config/database';
 import dotenv from 'dotenv';
+import cors from 'cors'
 
 import userRouter from './routes/userRoutes';
 import authRouter from './routes/authRoutes';
@@ -11,6 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 connectDB();
+
+app.use(cors({
+  credentials: true,
+  origin: "https://revastaff.vercel.app",
+  methods: ["GET","POST","PATCH","DELETE"],
+  allowedHeaders: ["X-CSRF-Token", "X-Requested-With", "Accept", "Accept-Version", "Content-Length", "Content-MD5", "Content-Type", "Date", "X-Api-Version"]
+}))
 
 app.use(express.json());
 
